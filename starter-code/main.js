dba66da9c9dcb4fc134181a3362bbfc3618cbce2
Erica Norby <ericanorby@Ericas-MBP.fios-router.home>
2017-01-06 15:57:12 -0500

    created alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,11 @@ var cardOne = "queen"
 var cardTwo = "king"
 var cardThree = "king"
 var cardFour = "queen"
+
+if (cardOne === cardFour) {
+	alert("You found a match!");
+} else if (cardTwo === cardThree) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
