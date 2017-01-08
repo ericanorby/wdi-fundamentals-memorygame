c3932933f087779ac271bf4368188dfca311add5
Erica Norby <ericanorby@Ericas-MacBook-Pro.local>
2017-01-07 21:19:46 -0500

    added cards in main.js, added subheading in index.html

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,30 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen"
-var cardTwo = "king"
-var cardThree = "king"
-var cardFour = "queen"
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "queen";
 
-if (cardOne === cardFour) {
+
+
+var createCards = function(num) {
+	var newBoard = document.getElementById('game-board');
+	for (var i = 0; i < num; i++) {
+		var addCard = document.createElement('div');
+		addCard.className = 'card';
+		newBoard.appendChild(addCard);
+	}
+}
+console.log(createCards(4));
+
+
+
+
+/*if (cardOne === cardFour) {
 	alert("You found a match!");
 } else if (cardTwo === cardThree) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
-}
+}*/
+
