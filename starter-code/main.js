3682b980164f6a66ee71c19e6e54e2644b969084
Erica Norby <ericanorby@gmail.com>
2017-01-15 16:56:01 -0500

    made game functional, added images

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,30 +1,56 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
 
-
-
-var createCards = function(num) {
+var createCards = function() {
 	var newBoard = document.getElementById('game-board');
-	for (var i = 0; i < num; i++) {
+	for (var i = 0; i < cards.length; i++) {
 		var addCard = document.createElement('div');
 		addCard.className = 'card';
-		newBoard.appendChild(addCard);
+		addCard.setAttribute('data-card',cards[i]);
+		addCard.addEventListener('click',flipCard);
+		newBoard.appendChild(addCard);	
+	}	
+}
+
+var flipCard = function() {
+	var thisCard = this.getAttribute('data-card');
+	cardsInPlay.push(thisCard);
+	if (thisCard === 'queen') {
+		this.innerHTML = '<img src="queen.jpg">';
+	} else {
+		this.innerHTML = '<img src="king.jpg">';
+	}
+	if (cardsInPlay.length === 2) {
+		setTimeout(isMatch, 200);	
+	} else if (cardsInPlay.length === 4) {
+		setTimeout(isMatchTwo, 200);
 	}
 }
-console.log(createCards(4));
 
+var isMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+		clearBoard();
+		cardsInPlay = [];
+	}
+}
 
+var isMatchTwo = function() {
+	if (cardsInPlay[2] === cardsInPlay[3]) {
+		alert("You matched all the cards! The board will be cleared.");
+		clearBoard();
+		cardsInPlay = [];
+	}
+}
 
+var clearBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	document.getElementsByClassName('card')[i].innerHTML=' ';
+	}	
+}
 
-/*if (cardOne === cardFour) {
-	alert("You found a match!");
-} else if (cardTwo === cardThree) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}*/
-
+createCards();
